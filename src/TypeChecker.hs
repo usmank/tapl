01b027ec1d1e36a14a11e06eff0aa564d5925250
Usman Khan <usmankhan9190@gmail.com>
2016-02-28 17:01:44 -0500

    Indentation tweaks.

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -7,9 +7,9 @@ import AST
 import Util
 
 typeOf :: Term -> Context -> Type
-typeOf (TmTrue _) ctx         = TyBool
-typeOf (TmFalse _) ctx        = TyBool
-typeOf (TmIf sp t1 t2 t3) ctx  =
+typeOf (TmTrue _) ctx = TyBool
+typeOf (TmFalse _) ctx = TyBool
+typeOf (TmIf sp t1 t2 t3) ctx =
   let t1Type  = typeOf t1 ctx
       t2Type  = typeOf t2 ctx
       t3Type  = typeOf t3 ctx
@@ -18,12 +18,12 @@ typeOf (TmIf sp t1 t2 t3) ctx  =
               then t2Type
               else err sp "Type error: Arms of conditional have different types."
         else err sp "Type error: Conditional guard not a boolean."
-typeOf (TmVar sp i) ctx        = fromMaybe (err sp "Type error: Var type lookup failed.") (getVarType i ctx)
+typeOf (TmVar sp i) ctx = fromMaybe (err sp "Type error: Var type lookup failed.") (getVarType i ctx)
 typeOf (TmAbs _ n typ t1) ctx =
   let ctx'    = addBinding n (VarBind typ) ctx
       t1Type  = typeOf t1 ctx'
   in  TyArrow typ t1Type
-typeOf (TmApp sp t1 t2) ctx    =
+typeOf (TmApp sp t1 t2) ctx =
   let t1Type  = typeOf t1 ctx
       t2Type  = typeOf t2 ctx
   in  case t1Type of
